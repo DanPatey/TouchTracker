449d180b9a7d477c1417691d6e8acd3056316b49
Dan Patey <danpatey@danpatey.com>
2016-07-03 20:28:50 -0700

    Cleaning up DrawView with some IBInspectables

--- TouchTracker/DrawView.swift
+++ TouchTracker/DrawView.swift
@@ -13,9 +13,27 @@ class DrawView: UIView {
     var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
     
+    @IBInspectable var finishedLineColor: UIColor = UIColor.blackColor() {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var currentLineColor: UIColor = UIColor.redColor() {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var lineThickness: CGFloat = 10 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
     func strokeLine(line: Line) {
         let path = UIBezierPath()
-        path.lineWidth = 10
+        path.lineWidth = lineThickness
         path.lineCapStyle = CGLineCap.Round
         
         path.moveToPoint(line.begin)
@@ -25,13 +43,13 @@ class DrawView: UIView {
     
     override func drawRect(rect: CGRect) {
         // Draw finished lines in black
-        UIColor.blackColor().setStroke()
+        finishedLineColor.setStroke()
         for line in finishedLines {
             strokeLine(line)
         }
         
         // Draw current line in read
-        UIColor.redColor().setStroke()
+        currentLineColor.setStroke()
         for (_, line) in currentLines {
             strokeLine(line)
         }
